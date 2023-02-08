c2a17ae95e57132533f8b971434df0767c65248b
nchris128 <nchris128@gmail.com>
2023-02-08 17:45:00 -0500

    Add files via upload

---
+++ mystyles.css
@@ -0,0 +1,73 @@
+body { background-color:#FFC425;
+      background-image: url(background.jpg);
+      color: #000000;
+      font-family: Arial, helvetica;;
+}
+#wrapper { width: 80%;
+           min-width: 700px;
+           max-width: 1024px;
+           margin-right: auto;
+           margin-left: auto;
+           background-color:#55aa55;
+           box-shadow: 3px 3px 3px #666666;
+}
+header { background-color: #231F20;
+         color: #ffffff;
+       font-family: Arial, helvetica;;
+}
+h1 { line-height: 200%;
+        text-align: center;
+    background-repeat: no-repeat;
+    background-position: right;
+    padding-left: 20px;
+    height: 72px;
+    margin-bottom: 0;
+background-color:##231F20;
+color:#ffffff;
+}
+nav { font-weight: bold;
+     padding: 5px;
+     float:left;
+     width: 160px;
+     padding: 20px 5px 0 20px;
+}
+nav a { text-decoration: none; }
+nav a:link { color: #000033; }
+nav a:visited { color: #344873 }
+nav a:hover { color: #FFFFFF; }
+nav ul { list-style-type: none;
+       margin: 0;
+       padding-left: 0;
+}
+h2 { color: #000000;
+    font-family: Arial, helvetica;
+}
+dt { color: #000000;
+    font-weight: bold;
+}
+.resort { color: #000000;
+          font-size: 1.2em;
+}
+footer { font-size: 0.70em;
+       text-align: center;
+       font-style: italic;
+       padding: 10px;
+       margin-left: 170px;
+       background-color: #FFFFFF;
+}
+h3 { color: #000000;
+}
+main { padding-left: 20px;
+        padding-right: 20px;
+        background-color: #FFFFFF;
+        margin-left: 170px;
+        padding-top: 1px;
+        padding-bottom: 1px;
+}
+main img { float: left;
+           padding-right: 20px;
+           padding-bottom: 20px;
+}
+main ul { list-style-position: inside; }
+.clear { clear: both; }
+header, nav, main, footer { display: block; }
